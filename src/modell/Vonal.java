513165127599eb1c7fe6009de31e5b6c849406ab
Yclaze <teralocsi@gmail.com>
2024-08-03 08:26:41 +0200

    String szinek, char stilusok

--- src/modell/Vonal.java
+++ src/modell/Vonal.java
@@ -8,6 +8,12 @@ public class Vonal {
    private String stilus;
    private enum Szinek {PIROS, KEK, ZOLD};
    private enum Stilusok {SZIMPLA, DUPLA, PONT};
+   private final String RED_ANSI = "\\u001b[31m";
+   private final String GREEN_ANSI = "\\u001b[32m";
+   private final String BLUE_ANSI = "\\u001b[34m";
+   private final char SZIMPLA = '-';
+   private final char DUPLA = '=';
+   private final char PONT = '.';
     
     public Vonal(int hossz) {        
         this(hossz, 0, Szinek.KEK, Stilusok.SZIMPLA);
