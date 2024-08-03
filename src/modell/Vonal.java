39ee2cb7b4897dc3be65361558a0840cf8e10d73
Yclaze <teralocsi@gmail.com>
2024-08-03 08:17:02 +0200

    private field, konstruktorok

--- src/modell/Vonal.java
+++ src/modell/Vonal.java
@@ -2,6 +2,29 @@ package modell;
 
 public class Vonal {
 
+   private int hossz;
+   private int eltolas;
+   private String szin;
+   private String stilus;
+   private enum Szinek {PIROS, KEK, ZOLD};
+   private enum Stilusok {SZIMPLA, DUPLA, PONT};
+    
+    public Vonal(int hossz) {        
+        this(hossz, 0, Szinek.KEK, Stilusok.SZIMPLA);
+    }
+    public Vonal(int hossz, int eltolas) {
+        this(hossz, eltolas, Szinek.KEK, Stilusok.SZIMPLA);
+    }
+    public Vonal(int hossz, int eltolas, Szinek szin) {
+        this(hossz, eltolas, szin, Stilusok.SZIMPLA);
+    }
+    public Vonal(int hossz, int eltolas, Szinek szin, Stilusok stilus) {
+        this.hossz = hossz;
+        this.eltolas = eltolas;
+        this.szin = szin.name();
+        this.stilus = stilus.name();
+    }
+
     
     
 }
