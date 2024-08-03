d8a55599412d7aa0135f24b4bcdacd51b9b398cf
Yclaze <teralocsi@gmail.com>
2024-08-03 10:19:28 +0200

    Lotto()

--- src/modell/MindentTudoGep.java
+++ src/modell/MindentTudoGep.java
@@ -3,4 +3,25 @@ package modell;
 
 public class MindentTudoGep {
     
+    private String lotto = "";
+    private String toto = "";
+    private String zeneCim = "";
+    private static final int L = 5;
+    private static final int T = 13;
+    
+    public MindentTudoGep(int mod){
+        lotto();
+        toto();
+        zene();
+    }
+
+    private void lotto() {
+        for (int i = 0; i < L; i++) {
+            if (i < L - 1) {
+                lotto += (Math.random() * 90 + 1) + ", ";
+            }else{
+                lotto += (Math.random() * 90 + 1) + ".";
+            }
+        }
+    }
 }
