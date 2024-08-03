f2e8cc608b28b78e01530d0ae31cade0d034925e
Yclaze <teralocsi@gmail.com>
2024-08-03 11:44:29 +0200

    getzeneCím()

--- src/main/OOProgram.java
+++ src/main/OOProgram.java
@@ -1,8 +1,13 @@
 
 package main;
 
+import modell.MindentTudoGep;
+
 public class OOProgram {
     public static void main(String[] args) {
-        
+        MindentTudoGep gep0 = new MindentTudoGep();
+        System.out.println(gep0.getLotto());
+        System.out.println(gep0.getToto());
+        System.out.println(gep0.getZeneCim());
     }
 }
