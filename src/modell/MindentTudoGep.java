bf1d443d0ea75c3214849892ff730ce2c19e5e5c
Yclaze <teralocsi@gmail.com>
2024-08-03 10:55:11 +0200

    Lotto() módosítás, TODO: kétszer fut le

--- src/modell/MindentTudoGep.java
+++ src/modell/MindentTudoGep.java
@@ -1,27 +1,63 @@
 
 package modell;
 
+import java.util.Arrays;
+
 public class MindentTudoGep {
     
     private String lotto = "";
     private String toto = "";
     private String zeneCim = "";
     private static final int L = 5;
-    private static final int T = 13;
+    private int[] lottoTomb = new int[L];
+    private static final int T = 14;
+    private int[] totoTomb = new int[T];
     
     public MindentTudoGep(int mod){
-        lotto();
-        toto();
-        zene();
+        getLotto();
+        //getToto();
+        //zene();
     }
 
-    private void lotto() {
-        for (int i = 0; i < L; i++) {
-            if (i < L - 1) {
-                lotto += (Math.random() * 90 + 1) + ", ";
-            }else{
-                lotto += (Math.random() * 90 + 1) + ".";
+    public String getLotto() {
+        lottoTombFeltoltes();
+        duplikacioEllenorzes();
+        rendezes();
+        for (int i = 0; i < lottoTomb.length; i++) {
+            this.lotto += lottoTomb[i] + ", ";
+        }
+        return this.lotto;
+    }
+
+    private void rendezes() {
+        Arrays.sort(lottoTomb, 0, lottoTomb.length);
+    }
+
+    private void duplikacioEllenorzes() {
+        for (int i : lottoTomb) {
+            for (int j : lottoTomb) {
+                if (i == j) {
+                    j = rnd(1, 90);
+                }
             }
         }
+    }
+
+    private void lottoTombFeltoltes() {
+        for (int i = 0; i < lottoTomb.length; i++) {
+            lottoTomb[i] = rnd(1, 90);
+        }
+    }
+
+    private int rnd(int min, int max) {
+        return (int)(Math.random()*max + min);
+    }
+
+    public void getToto() {
+        toto += "mérkőzések: ";
+        for (int i = 0; i < T; i++) {
+            toto+= i+1 + " ";
+        }
+        toto += "13+1";
     }
 }
